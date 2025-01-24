5296b55ae06445cd41fcd7cb172b41ca2399ccc9
ErickDominguez <erick_dominguez_santiago@outlook.es>
2025-01-24 15:57:00 -0600

    Se corrije ejercicio y se agregan comentarios

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,39 +1,44 @@
 package com.generation;
 
-public class Codigo4 {
+import java.util.Scanner;
 
-    Scanner s = new Scanner();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    String j1 = s.nextLine();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
-    String j2 = s.nextLine();
-    
-    if (j1 == j2)) {
-      System.out.println("Empate");
-    } else {
-      int g = 2;
-      switch(j1) {
-        case "piedra":
-          if (j2 == "tijeras") {
-            g = 1;
-          }
+public class Codigo4{
+	public static void main(String[] args) {//Se agregar clase main para correr el programa
+		
+		Scanner s =  new Scanner(System.in);// se agregar System.in
+	    
+		System.out.println("Turno del jugador 1 (introduzca piedra, papel o tijeras): " );
+	    
+	    String j1 = s.nextLine();
+	    
+	    System.out.println("Turno del jugador 2 (introduzca piedra, papel o tijeras): " );//cambia nombre del jugador
+	    //Se elimina la segunda instacia de Scanner
+	    
+	    String j2 = s.nextLine();  // se le entrega el valor en s
+	    
 
-        case "papel":
-          if (j2 == "piedra") {
-            g = 1;
-          
-        case "tijera":
-          if (j2.equals("papel")) {
-            g = 1;
-          }
-          break;
-        default:
-      }
-      System.out.println("Gana el jugador " + g);
-    }
-  
-  
+	    if(j1 == j2){
+		      System.out.println("Empate");
+		
+	    }else{
+	    	int g = 2;
+		    switch(j1) {
+		        case "piedra":
+		        if (j2 == "tijeras") {
+		            g = 1;
+		        }
+		        case "papel":
+		          if (j2 == "piedra") {
+		            g = 1;
+		          }// se agrega "}"
+		        case "tijera":
+		          if (j2 =="papel") {// cambia equals por "=="
+		            g = 1;
+		          }
+		          break;
+		        default: g = 2;
+		      }
+		      System.out.println("Gana el jugador " + g);
+		 }	  
+}
 }
